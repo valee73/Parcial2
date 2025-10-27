aea6fe3d7d46191a3f6fbf035b24068230bc0122
valee73 <98842330+valee73@users.noreply.github.com>
2025-10-27 16:24:06 -0600

    Add files via upload

---
+++ Ac5-P2.swift
@@ -0,0 +1,18 @@
+//
+//  Ac5-P2.swift
+//  HolaMundo
+//
+//  Created by WIN603 on 08/10/25.
+//
+
+import SwiftUI
+
+struct Ac5_P2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ac5_P2()
+}
